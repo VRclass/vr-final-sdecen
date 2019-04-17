21cf660045e878a5870c9ffa3aa3692b82026393
bl <blopez3@saic.edu>
2019-04-17 15:50:52 -0500

    Create LoadScene.cs

---
+++ Resources/LoadScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public Object theScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(theScene.name, LoadSceneMode.Single);
+    }
+}
